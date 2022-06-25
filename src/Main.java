4c4890e29f1908fcf8a7566be4b9e805b6d0eb63
Nikita <athlon64x2600@gmail.com>
2022-06-25 22:07:53 +0200

    Задание 2. Напишите метод, куда подаются два параметра: тип операционной системы (ОС) ( 0 — iOS или 1 — Android ) и год выпуска устройства. Если устройство старше текущего года, предложите ему установить lite-версию (облегченную версию).

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main (String[] args) {
         // Задание 1
         isYearLeap(2024);
+        // Задание 2
+        isVersionCorrect(1, 2022);
     }
     public static void isYearLeap(int year) {
 
@@ -9,6 +13,19 @@ public class Main {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void isVersionCorrect(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear < currentYear && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear == currentYear && clientOS == 1) {
+            System.out.println("Установите стандартную версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear < currentYear && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear == currentYear && clientOS == 0) {
+            System.out.println("Установите стандартную версию приложения для iOS по ссылке");
         }
     }
 }
