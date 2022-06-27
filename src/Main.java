66746077a9cce01e3677cdf68224ec49c7a4f434
Nikita <athlon64x2600@gmail.com>
2022-06-27 13:58:43 +0200

    Задание 3. Напишите программу, которая выдает сообщение в консоль:"Потребуется дней: " + срок доставки

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         isYearLeap(2024);
         // Задание 2
         isVersionCorrect(1, 2022);
+        // Задание 3
+        System.out.println("Потребуется дней доставки - " + deliveryDays(44));
     }
     public static void isYearLeap(int year) {
 
@@ -27,5 +29,21 @@ public class Main {
         } else if (clientDeviceYear == currentYear && clientOS == 0) {
             System.out.println("Установите стандартную версию приложения для iOS по ссылке");
         }
+    }
+    public static int deliveryDays(int distance) {
+        int days = 0;
+        if (distance >= 0 && distance < 20) {
+            days += 1;
+        }
+        if (distance >= 20 && distance < 60) {
+            days += 2;
+        }
+        if (distance >= 60 && distance < 100) {
+            days += 3;
+        }
+        if (distance >= 100) {
+            days += 4;
+        }
+        return days;
     }
 }
